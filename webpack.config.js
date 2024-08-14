75c6fd803158212d6f4eaf6a484442d5cb28902c
Karlo Cehulic <karlocehlic@gmail.com>
2024-08-14 16:10:58 +0200

    Fix wrong template propery in 'HTMLWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template:  "cache./src/template.html",
+            template:  "./src/template.html",
         })
     ],
 };
