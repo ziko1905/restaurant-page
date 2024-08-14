5b4bb394c3a744bd0a348f41d4cdde2ba057f8dc
Karlo Cehulic <karlocehlic@gmail.com>
2024-08-14 23:54:57 +0200

    Fix typos in 'index.js'

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import "./styles.css";
-import { functions, cleanContent } from "./load.js";
+import { Functions, cleanContent } from "./load.js";
 
 function functionalizeBtns() {
     const buttons = document.querySelectorAll("button")
 
     for (let btn of buttons) {
         btn.addEventListener("click", (e) => {
-            functions[`${e.target.id}`]()
+            Functions[`${e.target.id}`]()
         })
     }
 }
