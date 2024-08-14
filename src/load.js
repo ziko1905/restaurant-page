6b0751448bf0d1ab2d264fb05785bcd68b905d9e
Karlo Cehulic <karlocehlic@gmail.com>
2024-08-14 22:51:41 +0200

    Fix typo in text

--- src/load.js
+++ src/load.js
@@ -11,7 +11,7 @@ export function loadPage() {
     title.textContent = "Kebab paradise!";
     subtitle.textContent = "Experience Best Kebab In Town For Astonishing Prices!";
     image.src = kebab;
-    text.textContent = "Want to try our brand no kebab? No problem! Just come visit us at 'random address'! We are open 24/7 and love serving our customers best kebab in town! Only one as well! :)";
+    text.textContent = "Want to try our brand new kebab? No problem! Just come visit us at 'random address'! We are open 24/7 and love serving our customers best kebab in town! Only one as well! :)";
 
     content.appendChild(title);
     content.appendChild(subtitle);
