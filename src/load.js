6334a06023d2874b4b33d3f3f362e492b4cf08e5
Karlo Cehulic <karlocehlic@gmail.com>
2024-08-15 02:14:08 +0200

    Add dishes and basic stling for them

--- src/load.js
+++ src/load.js
@@ -32,19 +32,20 @@ function loadHome() {
 function loadMenu() {
     const div = document.createElement("div");
     const title = document.createElement("h1");
-    const subtitle = document.createElement("h3");
-    const image = document.createElement("img");
+    const dishList = document.createElement("ul");
     const text = document.createElement("p");
 
     div.className = "about"
-    title.textContent = "Kebab paradise!";
-    subtitle.textContent = "Experience Best Kebab In Town For Astonishing Prices!";
-    image.src = kebab;
+    title.textContent = "Our Menu!";
+    for (let n of ["Kebab", "Kebab Salad", "Kebab Pizza", "Kebab Tortilla", "Kebab Spaghetti"]) {
+        const dish = document.createElement("li");
+        dish.textContent = n;
+        dishList.appendChild(dish)
+    }
     text.textContent = "Want to try our brand new kebab? No problem! Just come visit us at 'random address'! We are open 24/7 and love serving our customers best kebab in town! Only one as well! :)";
 
     div.appendChild(title);
-    div.appendChild(subtitle);
-    div.appendChild(image);
+    div.appendChild(dishList);
     div.appendChild(text);
     content.appendChild(div)
 }
