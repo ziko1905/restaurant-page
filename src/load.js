02b29724617f711bc0a567613f5c881606345de2
Karlo Cehulic <karlocehlic@gmail.com>
2024-08-15 03:26:28 +0200

    Add content and styling to about

--- src/load.js
+++ src/load.js
@@ -61,20 +61,28 @@ function loadMenu() {
 function loadAbout() {
     const div = document.createElement("div");
     const title = document.createElement("h1");
-    const subtitle = document.createElement("h3");
-    const image = document.createElement("img");
-    const text = document.createElement("p");
 
     div.className = "about"
-    title.textContent = "Kebab paradise!";
-    subtitle.textContent = "Experience Best Kebab In Town For Astonishing Prices!";
-    image.src = kebab;
-    text.textContent = "Want to try our brand new kebab? No problem! Just come visit us at 'random address'! We are open 24/7 and love serving our customers best kebab in town! Only one as well! :)";
-
+    title.textContent = "Contact: ";
     div.appendChild(title);
-    div.appendChild(subtitle);
-    div.appendChild(image);
-    div.appendChild(text);
+
+    for (let n = 0; n < 3; n++) {
+        const card = document.createElement("div");
+        const name = document.createElement("h2");
+        const number = document.createElement("h4");
+        const role = document.createElement("h4");
+
+        card.className = "card"
+        name.textContent = ["Kebaoman", "Kebabochef", "Kebabotas"][n]
+        number.textContent = ["314-159-2653", "271-282-1828", "141-142-1356"][n]
+        role.textContent = ["Our awesome manager!", "Our talented chef!", "Our favorite customer!"][n]
+
+        card.appendChild(name);
+        card.appendChild(number);
+        card.appendChild(role);
+        div.appendChild(card);
+    }
+
     content.appendChild(div)
 }
 
